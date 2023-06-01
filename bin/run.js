407fb96a0cc67d241518861474830ae220d98038
zui4yi1 <xinlle.01@163.com>
2023-06-01 17:15:27 +0800

    增加进度条

--- bin/run.js
+++ bin/run.js
@@ -1,9 +1,17 @@
 #! /usr/bin/env node
+
+
+var ProgressBar = require('progress');
+var bar = new ProgressBar('编绎中[:bar]:percent', { total: 100,width:20,clear:false });
+
+
 const dirName = process.argv[2];
 const cwd = process.cwd();
 // 1. 生成公共样式
 const { commonClzExec } = require('../lib/compile-common-clz/conf/index');
-commonClzExec(cwd, dirName);
+commonClzExec(cwd, dirName, bar);
+bar.tick(10)
+
 
 // 2. 生成原子样式
 const atomConfs = require(`${cwd}/${dirName}`);
@@ -13,4 +21,4 @@ const ac = new AtomCompiler(atomConfs.theme, atomConfs.isMutilTheme, cwd, dirNam
 atomConfs.list.forEach(item=>{
     ac.addAtom(item)
 })
-ac.runAtom()
+ac.runAtom(bar)
